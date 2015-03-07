12bbe35e563b91866409967253bbe82c7b610c96
Bedrockminer <minerbedrock@gmail.com>
2015-03-07 13:21:28 +0100

    Tutorial TileEntity: Inventory
    * Added an Inventory for the TE
    
    Signed-off-by: Bedrockminer <minerbedrock@gmail.com>

--- Projects/src/java/com/bedrockminer/tutorial/blocks/BlockTileEntity.java
+++ Projects/src/java/com/bedrockminer/tutorial/blocks/BlockTileEntity.java
@@ -1,12 +1,13 @@
 package com.bedrockminer.tutorial.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
 
 import com.bedrockminer.tutorial.Main;
@@ -16,10 +17,10 @@ public class BlockTileEntity extends BlockContainer {
 	protected BlockTileEntity(String unlocalizedName) {
 		super(Material.iron);
 		this.setBlockName(unlocalizedName);
-        this.setBlockTextureName(Main.MODID + ":" + unlocalizedName);
-        this.setHardness(12.0F);
-        this.setHarvestLevel("pickaxe", 2);
-        this.setCreativeTab(CreativeTabs.tabDecorations);
+		this.setBlockTextureName(Main.MODID + ":" + unlocalizedName);
+		this.setHardness(12.0F);
+		this.setHarvestLevel("pickaxe", 2);
+		this.setCreativeTab(CreativeTabs.tabDecorations);
 	}
 
 	@Override
@@ -28,12 +29,39 @@ public class BlockTileEntity extends BlockContainer {
 	}
 
 	@Override
-	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
-	    if (!world.isRemote && world.getTileEntity(x,y,z) != null && world.getTileEntity(x,y,z) instanceof TileTutorial) {
-	        String message = ((TileTutorial)world.getTileEntity(x,y,z)).getValue();
-	        MinecraftServer.getServer().getConfigurationManager().sendChatMsg(new ChatComponentText(message));
-	        ((TileTutorial)world.getTileEntity(x,y,z)).setValue(player.getCommandSenderName() + "@" + MinecraftServer.getSystemTimeMillis());
-	    }
-	    return true;
+	public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
+		if (!world.isRemote && world.getTileEntity(x, y, z) != null && world.getTileEntity(x, y, z) instanceof TileTutorial) {
+			TileTutorial te = (TileTutorial) world.getTileEntity(x, y, z);
+			for (int i = 0; i < te.getSizeInventory(); ++i) {
+				ItemStack itemstack = te.getStackInSlot(i);
+
+				if (itemstack != null) {
+					EntityItem entityitem;
+
+					for (; itemstack.stackSize > 0; world.spawnEntityInWorld(entityitem)) {
+						float vX = world.rand.nextFloat() * 0.8F + 0.1F;
+						float vY = world.rand.nextFloat() * 0.8F + 0.1F;
+						float vZ = world.rand.nextFloat() * 0.8F + 0.1F;
+						int size = world.rand.nextInt(21) + 10;
+
+						if (size > itemstack.stackSize) {
+							size = itemstack.stackSize;
+						}
+
+						itemstack.stackSize -= size;
+						entityitem = new EntityItem(world, x + vX, y + vY, z + vZ, new ItemStack(itemstack.getItem(), size, itemstack.getItemDamage()));
+						float motion = 0.05F;
+						entityitem.motionX = (float) world.rand.nextGaussian() * motion;
+						entityitem.motionY = (float) world.rand.nextGaussian() * motion + 0.2F;
+						entityitem.motionZ = (float) world.rand.nextGaussian() * motion;
+
+						if (itemstack.hasTagCompound()) {
+							entityitem.getEntityItem().setTagCompound((NBTTagCompound) itemstack.getTagCompound().copy());
+						}
+					}
+				}
+			}
+		}
+		super.breakBlock(world, x, y, z, block, meta);
 	}
 }
--- Projects/src/java/com/bedrockminer/tutorial/blocks/TileTutorial.java
+++ Projects/src/java/com/bedrockminer/tutorial/blocks/TileTutorial.java
@@ -1,29 +1,128 @@
 package com.bedrockminer.tutorial.blocks;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 
-public class TileTutorial extends TileEntity {
+public class TileTutorial extends TileEntity implements IInventory {
 
-	private String myString;
+	private ItemStack[] inventory = new ItemStack[this.getSizeInventory()];
 
-	public String getValue() {
-	    return this.myString;
+	@Override
+	public int getSizeInventory() {
+		return 9;
 	}
 
-	public void setValue(String value) {
-	    this.myString = value;
+	@Override
+	public ItemStack getStackInSlot(int slot) {
+		if (slot < this.getSizeInventory() && slot >= 0)
+			return this.inventory[slot];
+		return null;
+	}
+
+	@Override
+	public ItemStack decrStackSize(int slot, int amount) {
+		if (this.getStackInSlot(slot) != null) {
+			ItemStack stack;
+			if (this.getStackInSlot(slot).stackSize <= amount) {
+				stack = this.getStackInSlot(slot);
+				this.setInventorySlotContents(slot, null);
+				this.markDirty();
+				return stack;
+			} else {
+				stack = this.getStackInSlot(slot).splitStack(amount);
+				if (this.getStackInSlot(slot).stackSize == 0)
+					this.setInventorySlotContents(slot, null);
+				this.markDirty();
+				return stack;
+			}
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public ItemStack getStackInSlotOnClosing(int slot) {
+		ItemStack stack = this.getStackInSlot(slot);
+		this.setInventorySlotContents(slot, null);
+		return stack;
+	}
+
+	@Override
+	public void setInventorySlotContents(int slot, ItemStack stack) {
+		if (slot < this.getSizeInventory() && slot >= 0)
+			this.inventory[slot] = stack;
+	}
+
+	@Override
+	public String getInventoryName() {
+		return "inventory.tiletutorial.title";
+	}
+
+	@Override
+	public boolean hasCustomInventoryName() {
+		return false;
+	}
+
+	@Override
+	public int getInventoryStackLimit() {
+		return 64;
+	}
+
+	@Override
+	public boolean isUseableByPlayer(EntityPlayer player) {
+		return true;
+	}
+
+	@Override
+	public void openInventory() { }
+
+	@Override
+	public void closeInventory() { }
+
+	@Override
+	public boolean isItemValidForSlot(int slot, ItemStack stack) {
+		return true;
 	}
 
 	@Override
 	public void readFromNBT(NBTTagCompound nbt) {
 		super.readFromNBT(nbt);
-		this.myString = nbt.getString("playerName");
+
+		NBTTagList itemList = nbt.getTagList("Items", 10);
+        this.inventory = new ItemStack[this.getSizeInventory()];
+
+        for (int i = 0; i < itemList.tagCount(); ++i)
+        {
+            NBTTagCompound stackTag = itemList.getCompoundTagAt(i);
+            int j = stackTag.getByte("Slot") & 255;
+
+            if (j >= 0 && j < this.getSizeInventory())
+            {
+                this.setInventorySlotContents(j, ItemStack.loadItemStackFromNBT(stackTag));
+            }
+        }
 	}
 
 	@Override
 	public void writeToNBT(NBTTagCompound nbt) {
 		super.writeToNBT(nbt);
-		nbt.setString("playerName", this.myString);
+		NBTTagList itemList = new NBTTagList();
+
+        for (int i = 0; i < this.getSizeInventory(); ++i)
+        {
+            if (this.getStackInSlot(i) != null)
+            {
+                NBTTagCompound stackTag = new NBTTagCompound();
+                stackTag.setByte("Slot", (byte)i);
+                this.getStackInSlot(i).writeToNBT(stackTag);
+                itemList.appendTag(stackTag);
+            }
+        }
+
+        nbt.setTag("Items", itemList);
 	}
 }
